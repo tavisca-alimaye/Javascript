7b45642880f67d4840ffe6f9b3072f7b8b26afbd
Arnav Limaye <aal4493@gmail.com>
2014-07-28 09:57:18 +0530

    Pushing bouncing ball using canvas

--- Bouncing Ball/bounce-ball.js
+++ Bouncing Ball/bounce-ball.js
@@ -1,8 +1,24 @@
+var ctx;
+var x = 30;
+var y = 240;
+var dx = 5;
+var dy = 5;
 var init = function(){
-	var can = document.getElementById("myCanvas");
-	var ctx = can.getContext("2d");
+	var can = document.getElementById("ballBoundry");
+	ctx = can.getContext('2d');
+	setInterval(draw,20);
+}
+var draw = function(){
+	ctx.clearRect(0,0,600,500);
 	ctx.beginPath();
 	ctx.fillStyle = "#FF0000";
-	ctx.arc(30,270,30,0,2*Math.PI);
+	ctx.arc(x,y,30,0,2*Math.PI);
+	ctx.closePath();
 	ctx.fill();
+	if(x > 570 || x < 30)
+		dx = -dx;
+	if(y > 470 || y < 30)
+		dy = - dy;
+	x += dx;
+	y += dy;
 }
